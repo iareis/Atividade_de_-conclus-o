fbcc225d194deea91412528cfaed747180adf838
Iuri Albuquerque Reis <iareis@gmail.com>
2020-11-30 01:34:45 -0300

    dupplic

---
+++ iareis_/app/controllers/oldHome.php
@@ -0,0 +1,21 @@
+<!-- <?php
+use Controller;
+
+class Home extends Controller
+{
+    // Index of the home page (localhost/home(/index))
+    public function index($param1 = '', $param2 = '', $param3 = '')
+    {
+        // Initialize Alunos model
+        $test = $this->model('Alunos');
+
+        // Call function from the model
+        $testData = $test->getAlunosFunction();
+
+        $this->view('home/index', [
+            'test' => $testData,
+            'parameters' => [$param1, $param2, $param3],
+        ]);
+    }
+}
+?> -->
